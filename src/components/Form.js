79d812ccd068d6cb6ab309278f7fc8ba169523b7
Victor Muturi <muturivictor21@gmail.com>
2024-02-03 14:17:20 +0300

    Completeed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-function Form(props) {
+function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [errors, setErrors] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,49 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+   
+    const newErrors = [];
+    if (firstName.trim() === "") {
+      newErrors.push("First name is required!");
+    }
+    if (lastName.trim() === "") {
+      newErrors.push("Last name is required!");
+    }
+
+    
+    if (newErrors.length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
+    
+    console.log("Form submitted with data:", { firstName, lastName });
+
+    
+    setFirstName("");
+    setLastName("");
+   
+    setErrors([]);
+  }
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      
+      {errors.length > 0 &&
+        errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))}
+    </div>
   );
 }
 
